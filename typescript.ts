ccabb5ff6746a51473d8cc662c16f84cf4d7cf15
Priscilla Souza <priscillaxtp@edu.unirio.br>
2022-10-19 20:58:27 -0300

    first commit

---
+++ typescript.ts
@@ -0,0 +1,15 @@
+const bubbleSort = (arr: number[]): number[] => {
+    const len = arr.length;
+
+    for (let i = 0; i < len; i++) { //O(n)
+        for (let j = 0; j < len; j++) { // O(n)
+            if (arr[j] > arr[j + 1]) { 
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+            }
+        }
+    }
+
+    return arr;
+}
+
+console.log(bubbleSort([7,3,9,2,1,5,13,2]))
