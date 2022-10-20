cf80f5c2ce168bfb1b50c905e93c2a79ece951b7
Priscilla Souza <priscillaxtp@edu.unirio.br>
2022-10-20 10:34:11 -0300

    Complexidade do fibonacci php

--- questao02/fibonacci.php
+++ questao02/fibonacci.php
@@ -1,16 +1,12 @@
 <?php
-// PHP Program to find n'th
-// fibonacci Number
-
+// Programa para descobrir o número x da sequência fibonacci
+// Complexidade: O(log n), pois calcular phi ^ n requer tempo logarítmico
 function fib($n)
 {
 	$phi = (1 + sqrt(5)) / 2;
-	return round(pow($phi, $n) / sqrt(5));
+	return round(pow($phi, $n) / sqrt(5)); // O(log n)
 }
 
-// Driver Code
 $n = 9;
 echo fib($n) ;
-
-// This code is contributed by Ryuga
 ?>
